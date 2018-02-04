89f7b6207281357a5d4f4979d54dcff02044bb72
PeterChen <747853229@qq.com>
2018-02-04 11:03:56 +0800

    CombineReducers in index.js

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,10 @@
+// a reducer taks in two things:
+// * the action
+// * copy of current state
+
+function posts(state = [], action) {
+    console.log(state, action)
+    return state
+}
+
+export default posts
